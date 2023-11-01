ab1597e44579a05a81116a52a23afafe421f4ab3
daniel141989 <daniel.riveras@virginiogomez.cl>
2023-10-31 23:27:11 -0300

    logo udec prueba

--- views/template/header.php
+++ views/template/header.php
@@ -18,7 +18,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Sistema de Turnos</title>
+    <title>Sistema de Turnos UDEC</title>
 
     <meta name="description" content="" />
 
