54d1d61d7e3416cedbaa6aae05c3033bb211db1a
daniel141989 <daniel.riveras@virginiogomez.cl>
2023-10-31 23:38:11 -0300

    login de la pagina

--- index.php
+++ index.php
@@ -7,86 +7,43 @@ if($cerrar_session){
 }
 ?>
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style customizer-hide"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="../assets/"
-  data-template="vertical-menu-template-free"
->
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <section class="vh-100" style="background-color: #003660;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+                            <img src="img/campus_los_angeles.png" width="200" alt="Campus Los Angeles Logo">
 
-    <title>Sistema de Digitur</title>
+                            <h2 class="mb-3">Sistema de Gestión de Turnos DISE</h2>
 
-    <meta name="description" content="" />
+                            <div class="form-group mb-4">
+                                <label for="Email">Correo Electrónico</label>
+                                <input type="email" id="Email" class="form-control form-control-lg" />
+                            </div>
 
-    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+                            <div class="form-group mb-4">
+                                <label for="password">Contraseña</label>
+                                <input type="password" id="passwordlogin" class="form-control form-control-lg" />
+                            </div>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
-    <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
-    <link rel="stylesheet" href="assets/css/sweetalert2.min.css"/>
-  </head>
-  <body>
-    <div class="container-xxl">
-      <div class="authentication-wrapper authentication-basic container-p-y">
-        <div class="authentication-inner">
-          <div class="card">
-            <div class="card-body">
-              <div class="app-brand justify-content-center">
-              </div>
-              <h4 class="mb-2">Bienvenidos Modo Turno</h4>
-              <p class="mb-4">Please sign-in to your account and start the adventure</p>
-              <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
-                <div class="mb-3">
-                  <label for="email" class="form-label">Usuario</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="usuariologin"
-                    name="usuariologin"
-                    autofocus
-                  />
+                            <button class="btn btn-lg" type="submit" style="background-color: #ff9800" OnClick="Login()">Iniciar Sesión</button>
+
+                            <hr class="my-4">
+                        </div>
+                    </div>
                 </div>
-                <div class="mb-3 form-password-toggle">
-                  <div class="d-flex justify-content-between">
-                    <label class="form-label" for="password">Contraseña</label>
-                  </div>
-                  <div class="input-group input-group-merge">
-                    <input
-                      type="password"
-                      id="passwordlogin"
-                      class="form-control"
-                      name="passwordlogin"
-                    />
-                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                  </div>
-                </div>
-                <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="button" OnClick="Login()">Iniciar Session</button>
-                </div>
-              </form>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-    <script src="assets/js/sweetalert2.all.min.js"></script>
-    <script src="assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="assets/vendor/js/bootstrap.js"></script>
-    <script src="controllers/loginController.js"></script>
-  </body>
+    </section>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
