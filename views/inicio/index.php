e006970303ae48b04b56cdeae40b61cb87cedf0b
daniel141989 <daniel.riveras@virginiogomez.cl>
2023-10-31 21:11:21 -0300

    actualizacion por daniel

--- views/inicio/index.php
+++ views/inicio/index.php
@@ -43,6 +43,7 @@ require "../template/header.php";
                 </div>
               </div>
             </div>
+            <div><h1>titulo</h1></div>
 <?php
 require "../template/footer.php";
 ?>
