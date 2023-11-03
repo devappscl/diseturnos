496ba36562d29ce471d48292e78f705a55458c83
daniel141989 <daniel.riveras@virginiogomez.cl>
2023-11-03 00:36:27 -0300

    estudiantes d

--- views/clientes/index.php
+++ views/clientes/index.php
@@ -3,7 +3,7 @@ require "../template/header.php";
 ?>
 <div class="container-xxl flex-grow-1 container-p-y">
               <div class="card mb-4">
-                <h5 class="card-header">Clientes <button type="button" Onclick="modalagregar()" class="btn btn-primary float-right">Agregar Cliente</button></h5>
+                <h5 class="card-header">Estudiantes <button type="button" Onclick="modalagregar()" class="btn btn-primary float-right">Agregar Estudiante</button></h5>
                 <div class="card-body">
                 <?php include("agregar_cliente.php"); ?>
                 <div class="card-datatable table-responsive pt-0">
